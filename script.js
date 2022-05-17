8ddff21a20b5324fb95f8530c04b350d3d0610b8
Phan Van Binh <akiphan99@gmail.com>
2022-05-17 10:59:13 +0700

    push code

---
+++ script.js
@@ -0,0 +1,40 @@
+
+// Bài tập 1:
+// Input: tiền lương 100000, số ngày làm việc
+var salaryPerDay = 100000
+var workingDays = 30
+var totalSalary = salaryPerDay * workingDays
+console.log("Tong luong la: ",totalSalary)
+
+// Bai tap 2:
+var num1 = 10
+var num2 = 3
+var num3 = 5
+var num4 = 6
+var num5 = 8;
+
+var average = (num1 + num2 + num3 + num4 + num5) / 5
+console.log("Gia tri trung binh cua 5 so la: ", average)
+
+// Bai tap 3:
+var exchangeRate = 23500
+var usd = 2
+var exchange = exchangeRate * usd
+
+console.log("So tien sau quy doi", exchange)
+
+// Bai tap 4: 
+var width = 20
+var height = 30
+var area = width * height;
+var perimeter = (width + height) / 2
+console.log("Dien tich hinh chu nhat la: ", area)
+console.log("Chu vi hinh chu nhat la: ", perimeter)
+
+// Bai tap 5
+
+var num = 15
+var num2 = num % 10
+var num1 = Math.floor(num / 10)
+var sumOfNum = num1 + num2
+console.log("Tong hai chu so la: ", sumOfNum)
